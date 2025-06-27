ac95a3efe7e4714db472933bbd0b3238f9cf2079
cocopuff <77523389+420queen@users.noreply.github.com>
2025-06-27 17:26:02 +0200

    Serve JS/CSS locally for offline play

--- js/minimap.js
+++ js/minimap.js
@@ -7,8 +7,8 @@ function mminitialize() {
 
     mymap.setView([30, 10], 1);
 
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+    L.tileLayer('/tiles/{z}/{x}/{y}.png', {
+        attribution: 'Offline map',
         maxZoom: 18
     }).addTo(mymap);
 
