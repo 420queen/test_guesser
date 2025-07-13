9dc8aa3808fcb326d9fd6ed3b5f16f421c358f4f
cocopuff <77523389+420queen@users.noreply.github.com>
2025-07-13 22:45:13 +0200

    Use offline assets

--- js/minimap.js
+++ js/minimap.js
@@ -7,10 +7,10 @@ function mminitialize() {
 
     mymap.setView([30, 10], 1);
 
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-        maxZoom: 18
-    }).addTo(mymap);
+    const pmLayer = new protomaps.PMTilesLayer({
+        url: 'offline_assets/planet_z8.pmtiles'
+    });
+    pmLayer.addTo(mymap);
 
     guess2 = L.marker([-999, -999]).addTo(mymap);
     guess2.setLatLng({lat: -999, lng: -999});
