2cc15ca75f8753872a625ff50766b5fe7947f5bb
cocopuff <77523389+420queen@users.noreply.github.com>
2025-07-14 04:16:28 +0200

    Update roundmap.js

--- js/roundmap.js
+++ js/roundmap.js
@@ -1,14 +1,12 @@
-//
-// End of round map
-//
-
 function rminitialize() {
-    roundmap = L.map("roundMap").setView([30, 10], 1);
+    roundmap = L.map("roundMap").setView([30, 8], 1);
 
-    const pmLayer = new protomaps.PMTilesLayer({
-        url: 'offline_assets/planet_z8.pmtiles'
-    });
-    pmLayer.addTo(roundmap);
+    protomapsL.leafletLayer({
+        url: 'offline_assets/planet_z8.pmtiles',
+        flavor: "light",
+        lang: "fr",
+        attribution: "© OpenStreetMap, © Protomaps"
+    }).addTo(roundmap);
 
     var guessIcon = L.icon({
         iconUrl: "img/guess.png",
@@ -20,17 +18,21 @@ function rminitialize() {
         iconAnchor: [45, 90]
     });
 
-    guess = L.marker([-999, -999], {
-        icon: guessIcon
-    }).addTo(roundmap)
+    // Set markers to invisible positions initially
+    guess = L.marker([0, 0], { icon: guessIcon, opacity: 0 }).addTo(roundmap);
+    actual = L.marker([0, 0], { icon: actualIcon, opacity: 0 }).addTo(roundmap);
 
-    actual = L.marker([-999, -999], {
-        icon: actualIcon
-    }).addTo(roundmap)
+    // Only place markers and fit view if both coordinates are defined
+    if (window.guessLatLng && window.actualLatLng) {
+        guess.setLatLng(window.guessLatLng).setOpacity(1);
+        actual.setLatLng(window.actualLatLng).setOpacity(1);
 
-    guess.setLatLng(window.guessLatLng);
-    actual.setLatLng(window.actualLatLng);
-    roundmap.fitBounds(L.latLngBounds(guess.getLatLng(), actual.getLatLng()), {
-        padding: [50, 50]
-    });
-};
+        roundmap.fitBounds(
+            L.latLngBounds(
+                guess.getLatLng(),
+                actual.getLatLng()
+            ),
+            { padding: [50, 50] }
+        );
+    }
+}
