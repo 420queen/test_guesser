ec7448fd0673e3cdd345f5d8f6b179f53e53b226
cocopuff <77523389+420queen@users.noreply.github.com>
2025-07-15 10:00:11 +0200

    Update roundmap.js

--- js/roundmap.js
+++ js/roundmap.js
@@ -1,38 +1,37 @@
-//
-// End of round map
-//
-
 function rminitialize() {
     roundmap = L.map("roundMap").setView([30, 10], 1);
 
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-        maxZoom: 18
+    // Use online Protomaps vector tiles
+    protomapsL.leafletLayer({
+        url: 'https://tiles.protomaps.com/tiles/v3/{z}/{x}/{y}.pbf',
+        flavor: 'light',
+        lang: 'fr',
+        attribution: '© OpenStreetMap, © Protomaps'
     }).addTo(roundmap);
 
+    // Define custom icons
     var guessIcon = L.icon({
         iconUrl: "img/guess.png",
-        iconSize: [60, 60],        // Nouvelle taille
-        iconAnchor: [30, 60]       // Centre bas
+        iconSize: [60, 60],
+        iconAnchor: [30, 60] // center bottom
     });
 
     var actualIcon = L.icon({
         iconUrl: "img/actual.png",
-        iconSize: [60, 60],        // Nouvelle taille
-        iconAnchor: [30, 60]       // Centre bas
+        iconSize: [60, 60],
+        iconAnchor: [30, 60]
     });
 
-    guess = L.marker([-999, -999], {
-        icon: guessIcon
-    }).addTo(roundmap)
+    // Place markers
+    guess = L.marker([-999, -999], { icon: guessIcon }).addTo(roundmap);
+    actual = L.marker([-999, -999], { icon: actualIcon }).addTo(roundmap);
 
-    actual = L.marker([-999, -999], {
-        icon: actualIcon
-    }).addTo(roundmap)
-
+    // Update positions
     guess.setLatLng(window.guessLatLng);
     actual.setLatLng(window.actualLatLng);
+
+    // Fit bounds
     roundmap.fitBounds(L.latLngBounds(guess.getLatLng(), actual.getLatLng()), {
         padding: [50, 50]
     });
-};
+}
